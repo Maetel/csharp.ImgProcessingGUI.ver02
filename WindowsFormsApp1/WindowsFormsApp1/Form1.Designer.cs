b25ff2b7434bcdeacf102afd251b93b46ac716c4
J. Hwang <iamjam4944@gmail.com>
2018-05-02 05:30:49 +0900

    ver0.2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Display = new System.Windows.Forms.TextBox();
+            this.Convert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Status = new System.Windows.Forms.Label();
+            this.Initialize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Display
+            // 
+            this.Display.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Display.Location = new System.Drawing.Point(12, 52);
+            this.Display.Multiline = true;
+            this.Display.Name = "Display";
+            this.Display.ReadOnly = true;
+            this.Display.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Display.Size = new System.Drawing.Size(655, 309);
+            this.Display.TabIndex = 0;
+            // 
+            // Convert
+            // 
+            this.Convert.BackColor = System.Drawing.SystemColors.Info;
+            this.Convert.Font = new System.Drawing.Font("HelveticaNeueLT Pro 43 LtEx", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Convert.Location = new System.Drawing.Point(232, 367);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(222, 41);
+            this.Convert.TabIndex = 1;
+            this.Convert.Text = "Convert";
+            this.Convert.UseVisualStyleBackColor = false;
+            this.Convert.Click += new System.EventHandler(this.Convert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("HelveticaNeueLT Pro 53 Ex", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(281, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 27);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File list";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Status
+            // 
+            this.Status.AutoSize = true;
+            this.Status.Location = new System.Drawing.Point(536, 384);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(131, 12);
+            this.Status.TabIndex = 3;
+            this.Status.Text = "Drag and drop images";
+            this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Initialize
+            // 
+            this.Initialize.BackColor = System.Drawing.SystemColors.Window;
+            this.Initialize.Font = new System.Drawing.Font("HelveticaNeueLT Pro 43 LtEx", 9F);
+            this.Initialize.Location = new System.Drawing.Point(460, 367);
+            this.Initialize.Name = "Initialize";
+            this.Initialize.Size = new System.Drawing.Size(70, 41);
+            this.Initialize.TabIndex = 4;
+            this.Initialize.Text = "Initialize";
+            this.Initialize.UseVisualStyleBackColor = false;
+            this.Initialize.Click += new System.EventHandler(this.Initialize_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(679, 416);
+            this.Controls.Add(this.Initialize);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.Display);
+            this.Name = "Form1";
+            this.Text = "ImageConverter ver0.2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Display;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.Button Initialize;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        const string convertedDir = "\\converted";
+        
+        List<string> imgFiles = new List<string>();
+
+        string filepath = "";
+        string savedPath = "";
+        bool isDropped = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //files.AddFirst(new string[] { "" });
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            isDropped = true;
+            string[] allFiles = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            int m_idx = 0;
+            this.Display.Text = "";
+
+            foreach ( string tempF in allFiles)
+            {
+                if ( tempF.Contains(".jpg"))
+                {
+                    imgFiles.Add(tempF);
+                }
+            }
+            
+            foreach (string filename in imgFiles)
+            {
+                this.Display.Text += ++m_idx + ". ";
+                for (int ii = filename.LastIndexOf('\\')+1 ; ii < filename.Length; ii++)
+                {
+                    this.Display.Text +=  filename[ii];
+                }
+                this.Display.Text += "\r\n";
+            }
+
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            if ( !isDropped )
+            {
+                MessageBox.Show("Please drag files.");
+                return;
+            }
+
+            this.Status.Text = "Converting...";
+
+            int m_idx = 0;
+
+            foreach ( string fileImg in imgFiles)
+            {
+                int m_pos = imgFiles[m_idx].LastIndexOf('\\');
+                filepath = imgFiles[m_idx++].Substring(0, m_pos);
+                //MessageBox.Show(m_idx + " " + filepath);
+                savedPath = filepath + convertedDir + '\\';
+                System.IO.Directory.CreateDirectory(savedPath);
+
+                string m_filename = "";
+                for (int ii = fileImg.LastIndexOf('\\') + 1; ii < fileImg.Length - 4; ii++)
+                {
+                    m_filename += fileImg[ii];
+                }
+
+
+                Bitmap i = Image.FromFile(fileImg) as Bitmap;
+                Rectangle cropRectL = new Rectangle(0, 0, i.Width/2, i.Height);
+                Rectangle cropRectR = new Rectangle(i.Width / 2, 0, i.Width / 2, i.Height);
+
+                Bitmap newImageL = new Bitmap(cropRectL.Width, cropRectL.Height);
+                Bitmap newImageR = new Bitmap(cropRectR.Width, cropRectR.Height);
+
+                Graphics gL = Graphics.FromImage(newImageL);
+                Graphics gR = Graphics.FromImage(newImageR);
+
+                gL.DrawImage(i, cropRectL.X, cropRectL.Y);
+                string fileL = savedPath + m_filename + "_L.jpg";
+                newImageL.Save(fileL, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                gR.DrawImage(i, cropRectL, cropRectR, GraphicsUnit.Pixel);
+                string fileR = savedPath + m_filename + "_R.jpg";
+                newImageR.Save(fileR, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                gL.Dispose();
+                gR.Dispose();
+                i.Dispose();
+                newImageL.Dispose();
+                newImageR.Dispose();
+
+            }
+
+            MessageBox.Show("Conversion Finished");
+
+            this.setLog();
+            this.initDisplay();
+
+            this.Status.Text = "Drag and drop images";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Initialize_Click(object sender, EventArgs e)
+        {
+            this.initDisplay();
+        }
+
+        private void initDisplay()
+        {
+            isDropped = false;
+            this.Display.Text = "";
+            imgFiles.Clear();
+        }
+
+        private void setLog()
+        {
+            //MessageBox.Show("set log");
+            string filePath = "c:\\ImageConversion0.2\\";
+            System.IO.Directory.CreateDirectory(filePath);
+            DateTime localdate = DateTime.Now;
+            string curTime = localdate.ToString().Replace(":", "-");
+            string fileName = (filePath + curTime + ".txt");
+
+            MessageBox.Show(fileName);
+
+            string data = curTime + "\r\n";
+
+            foreach( string s in imgFiles)
+            {
+                data += s + "\r\n";
+            }
+
+            System.IO.File.WriteAllText(fileName, data);
+        }
+    }
+}
